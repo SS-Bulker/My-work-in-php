5a64cb63f42c04a46e8c10fad4fbbb21e142ca71
SS-Bulker <juanrincon@protonmail.com>
2020-12-01 13:58:25 -0500

    agregando el orm de eleoquent en php nativo

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager as Database;
 
-class Database
-{
+$database = new Database();
 
-}
+$database->addConnection([
+    'driver'=>'mysql',
+    'host'=>env('DB_HOST') ?: 'localhost',
+    'database'=>env('DB_NAME') ?: 'php_pruebas',
+    'username'=>env('DB_USER') ?: 'root',
+    'password'=>env('DB_PASS') ?: '',
+    'charset'=>'utf8',
+    'prefix'=>''
+]);
+
+$database->setAsGlobal();
+$database->bootEloquent();
