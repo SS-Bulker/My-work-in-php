920bd47a6a331feaa16e67f22d8e71612449f624
SS-Bulker <juanrincon@protonmail.com>
2020-12-01 14:00:33 -0500

    Volviendo a configurar el indice como estaba

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 require_once 'Vendor/autoload.php';
 require_once 'App/Controllers/BaseController.php';
 require_once 'App/Controllers/ViewController.php';
-require_once 'App/Config/Database.php';
-require_once 'App/Models/Users.php';
 
 
 use App\Controllers\ViewController;
@@ -13,12 +11,6 @@ use App\Models\Users;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$usuarios = Users::where('id', 2)->get();
 
-dd($usuarios[0]->name);
-
-/*
 $obtenerPlantilla = new ViewController();
 $obtenerPlantilla->viewController();
-
-*/
